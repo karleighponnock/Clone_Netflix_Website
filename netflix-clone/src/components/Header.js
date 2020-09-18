9256baec58844d3c32fb994feab1176850deb415
karleighponnock <karleighponnock@gmail.com>
2020-09-18 12:52:21 -0400

    header netflix svg set

--- netflix-clone/src/components/Header.js
+++ netflix-clone/src/components/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import logo from '../svg/logo.svg'
 
 class Header extends Component {
     render(){
         return (
-            <div>
-                <h1>Header</h1>
+            <div className="header-container">
+                <div className="header-top"></div>
+                    <img src={logo} />
             </div>
         )
     }
