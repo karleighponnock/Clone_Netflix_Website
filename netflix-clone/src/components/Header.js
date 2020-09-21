8244210e3bb8c67c02413a654fe926408afc1cb1
karleighponnock <karleighponnock@gmail.com>
2020-09-21 14:03:08 -0400

    header styled with imported styled-component

--- netflix-clone/src/components/Header.js
+++ netflix-clone/src/components/Header.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react'
 import logo from '../svg/logo.svg'
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
 
 class Header extends Component {
     render() {
         return (
-            <div className="header-container">
+            <HeaderComponent className="header-container">
                 <div className="header-top">
                     <img src={logo} alt="logo" />
-                    <NavLink to="/about">Sign In</NavLink>
-                        {/* <NavLink className="signIn-btn"> Sign In</NavLink> */}
+                    <NavLink  className="signIn-btn" to="/about">Sign In</NavLink>
                 </div>
-            </div>
+            </HeaderComponent>
         );
     }
 }
 
 export default Header;
+
+//logo
+const Logo 
+//Header Container
+const HeaderComponent = styled.div`
+    .signIn-btn {
+        right: 0;
+        margin: 1.125rem 3% 0;
+        padding: 0.4375rem 1.0625rem;
+        font-weight: 400;
+        line-height: normal;
+        border-radius: 01875rem;
+        font-size: 1rem;
+        background: var(--main-red);
+        position: absolute;
+        translate: transform(-50%, -50%);
+        cursor: pointer;
+        transition: background 0.2s ease-in;
+        &:hover {
+            background: var(--main-red-hover)
+        };
+
+    }
+`;
