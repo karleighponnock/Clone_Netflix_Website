a36025e9b09b8b81b6c6068a891dda525217268b
karleighponnock <karleighponnock@gmail.com>
2020-09-23 12:56:17 -0400

    icons styled

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import TabDoor from './tabs_nav/TabDoor';
+import TabDevices from './tabs_nav/TabDevices';
 import '../css/TabsNav.css'
 
 class TabComponent extends Component {
@@ -13,7 +14,10 @@ class TabComponent extends Component {
                             <TabDoor>
                             </TabDoor>
                         </Tab>
-                        <Tab> Tab Link 2</Tab>
+                        <Tab> 
+                            <TabDevices>
+                            </TabDevices>
+                        </Tab>
                         <Tab> Tab Link 3</Tab>
                     </TabList>
                 </Tabs>
