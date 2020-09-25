f9fc0ead4f282cfda1217bde15544a0d036e97dc
karleighponnock <karleighponnock@gmail.com>
2020-09-24 20:23:25 -0400

    button styling

--- src/components/tabs_nav/TabContentOne.js
+++ src/components/tabs_nav/TabContentOne.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Img from '../../images/tab-1-pic.png'
 import { NavLink, Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { Button } from '../Button';
 
 function TabContentOne() {
     return (
         <TabContentContainer>
             <div className="container">
                 <div className="tab-content">
-                    <span>If you decide Netflix isn't for you - no problem.
+                    <div>
+                    <span style={{marginBotton: '2rem'}}>
+                        If you decide Netflix isn't for you - no problem.
                        No commitment. Cancel online anytime.</span>
                        <br />
-                        <button>try it now</button>
+                        <Button style={{marginTop: '2rem'}}> try it now </Button>
+                        </div>
                         <img src={Img} />
                 </div>
             </div>
@@ -24,6 +28,10 @@ export default TabContentOne;
 //Main Content Container
 const TabContentContainer = styled.div`
     background: var(--main-deep-dark);
+
+    .container {
+        margin: 0 10%;
+    }
 
     img {
         width: 31.875rem;
