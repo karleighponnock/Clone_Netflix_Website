c9cf950822b5626b09f66ca720dbf4fb6ef2dae9
karleighponnock <karleighponnock@gmail.com>
2020-10-10 15:21:53 -0400

    top message and import button

--- src/components/TabContentTwo.js
+++ src/components/TabContentTwo.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import { Button } from './Button'
 
-
-function TabContentTwo() {
-    return (
+    function TabContentTwo() {
+        return (
             <div className="tab-content">
                 <div className="tab-top-content">
-
+                    <span>Watch TV shows and movies anytime, anywhere - personalized for you.</span>
                 </div>
-               
+
             </div>
-    )
-}
+        )
+    }
 
 export default TabContentTwo;
