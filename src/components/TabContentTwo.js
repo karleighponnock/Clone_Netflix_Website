5c2307a591e6810ee019e38e8e2a98e2108eeebb
karleighponnock <karleighponnock@gmail.com>
2020-10-10 15:20:19 -0400

    tab componant 2 added

---
+++ src/components/TabContentTwo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function TabContentTwo() {
+    return (
+            <div className="tab-content">
+                <div className="tab-top-content">
+
+                </div>
+               
+            </div>
+    )
+}
+
+export default TabContentTwo;
