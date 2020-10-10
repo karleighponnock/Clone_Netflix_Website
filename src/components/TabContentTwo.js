4a08f07ab0be98e4d5feabf10c816c6e3ddcbd45
karleighponnock <karleighponnock@gmail.com>
2020-10-10 15:30:44 -0400

    begin styling of tab content 2

--- src/components/TabContentTwo.js
+++ src/components/TabContentTwo.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import { Button } from './Button'
+import styled from 'styled-components'
 
     function TabContentTwo() {
         return (
+            <TabContainer>
             <div className="tab-content">
                 <div className="tab-top-content">
-                    <span>Watch TV shows and movies anytime, anywhere - personalized for you.</span>
+                    <span>
+                        Watch TV shows and movies anytime, anywhere - personalized for you.
+                    </span>
                 <Button>try it now</Button>
                 </div>
-
             </div>
+            </TabContainer>
         )
     }
 
 export default TabContentTwo;
+
+
+// Main Tab Content Container
+const TabContainer = styled.div`
+background: var(--main-deep-dark);
+
+.tab-content{
+    margin: 015%;
+}
+
+// Tab Top Content
+.tab-top-content{
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    justify-content: center;
+    align-item: center;
+    padding: 2.5rem 0;
+}
+`;
