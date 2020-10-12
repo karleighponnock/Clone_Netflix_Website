2edf9b164d88168c45fb88cfc166b0c826c63190
karleighponnock <karleighponnock@gmail.com>
2020-10-12 17:05:40 -0400

    all images are correctly formatted. top and bottom portions of tab in place

--- src/components/TabContentTwo.js
+++ src/components/TabContentTwo.js
@@ -14,22 +14,23 @@ function TabContentTwo() {
                         Watch TV shows and movies anytime, anywhere - personalized for you.
                     </span>
                     <Button className="btn">try it now</Button>
-
+                </div>
+                <div className='tab-bottom-content'>
                     {/* //tv image container */}
                     <div>
-                        <img src={ImgTv} style={{width: '18.75rem'}}/>
+                        <img src={ImgTv} style={{ width: '18.75rem' }} />
                         <h3>Watch on your TV</h3>
                         <p>Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blue-ray players and more.</p>
                     </div>
-                     {/* //tablet image container */}
-                     <div>
-                        <img src={ImgTablet} style={{width: '18.75rem', paddingTop: '0.625rem'}}/>
+                    {/* //tablet image container */}
+                    <div>
+                        <img src={ImgTablet} style={{ width: '18.75rem', paddingTop: '0.625rem' }} />
                         <h3>Watch on your TV</h3>
                         <p>Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blue-ray players and more.</p>
                     </div>
                     {/* //macbook image container */}
                     <div>
-                        <img src={ImgMacbook} style={{width: '18.75rem', paddingTop: '0.625rem', paddingBottom:'0.625rem'}}/>
+                        <img src={ImgMacbook} style={{ width: '18.75rem', paddingTop: '0.625rem', paddingBottom: '0.625rem' }} />
                         <h3>Watch on your TV</h3>
                         <p>Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blue-ray players and more.</p>
                     </div>
@@ -68,4 +69,14 @@ span {
     grid-column: 10 / 12;
 }
 
+
+// Tab Botton Content
+
+.tab-bottom-content{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem;
+    text-align: center;
+    margin-top: 2rem;
+}
 `;
