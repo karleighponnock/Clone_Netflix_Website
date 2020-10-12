d204442004dd5abb3ea4fc1705198458a2fcdfc8
karleighponnock <karleighponnock@gmail.com>
2020-10-12 16:45:52 -0400

    image 1 added and placeholder for second two on tab two

--- src/components/TabContentTwo.js
+++ src/components/TabContentTwo.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import { Button } from './Button'
 import styled from 'styled-components'
+import ImgTv from '../images/tab-tv.png'
 
-    function TabContentTwo() {
-        return (
-            <TabContainer>
+function TabContentTwo() {
+    return (
+        <TabContainer>
             <div className="tab-content">
                 <div className="tab-top-content">
-                    <span style={{fontSize: '1.5rem'}}>
+                    <span style={{ fontSize: '1.5rem' }}>
                         Watch TV shows and movies anytime, anywhere - personalized for you.
                     </span>
-                <Button className="btn">try it now</Button>
+                    <Button className="btn">try it now</Button>
+
+                    {/* //tv image container */}
+                    <div>
+                        <img src={ImgTv} />
+                        <h3>Watch on your TV</h3>
+                        <p>Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blue-ray players and more.</p>
+                    </div>
+                     {/* //tablet image container */}
+                     <div>
+                        <img src={ImgTv} />
+                        <h3>Watch on your TV</h3>
+                        <p>Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blue-ray players and more.</p>
+                    </div>
+                    <div>
+                        <img src={ImgTv} />
+                        <h3>Watch on your TV</h3>
+                        <p>Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blue-ray players and more.</p>
+                    </div>
                 </div>
             </div>
-            </TabContainer>
-        )
-    }
+        </TabContainer>
+    )
+}
 
 export default TabContentTwo;
 
